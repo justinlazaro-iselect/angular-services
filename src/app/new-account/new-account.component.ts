beed026bb4189b47233b5907a737c8ff659f58c0
justinlazaro-iselect <justin.lazaro@iselect.com.au>
2020-02-26 13:26:58 +0800

    .

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,25 +6,16 @@ import { AccountsService } from "../accounts.service";
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
-  providers: [LoggingService, AccountsService]
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
-
   constructor(
     private loggingService: LoggingService,
     private accountService: AccountsService
   ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
     this.loggingService.logStatusChange(accountStatus);
     this.accountService.addAccount(accountName, accountStatus);
-
-    // console.log("A server status changed, new status: " + accountStatus);
   }
 }
